49937bb02392085614482df706a6f9297af249d6
Aaron Tamaddon <amtamaddon@gmail.com>
2015-07-09 17:25:46 +0000

    Changed config to =true

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module SimplecodecastsSaas
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config/application.rb
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
