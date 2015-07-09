c914d63180bfb5b2930b146cfc9bffc64e638665
Aaron Tamaddon <amtamaddon@gmail.com>
2015-07-09 17:28:51 +0000

    Removed extra config line

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ Bundler.require(*Rails.groups)
 module SimplecodecastsSaas
   class Application < Rails::Application
     
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
