e4d6b3b0b23923efe0b540a28a680328a13e9891
Aaron Tamaddon <amtamaddon@gmail.com>
2015-07-09 17:00:29 +0000

    Tried moving config line in application.rb to get heroku to work

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# config/application.rb
-config.assets.initialize_on_precompile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +10,10 @@ Bundler.require(*Rails.groups)
 
 module SimplecodecastsSaas
   class Application < Rails::Application
+    
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
